159fd94c5bfdb918b07fbd67a1b9a17738df49f8
Techwolf78 <mrajaypawar207@gmail.com>
2025-08-18 18:15:18 +0530

    push

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,7 @@ export default function App() {
   }
 
   return (
-    <Router>
+    <Router basename='/'>
       {user && (
         <header className="bg-white shadow-sm">
           <div className="max-w-7xl mx-auto px-4 py-3 flex justify-between items-center">
